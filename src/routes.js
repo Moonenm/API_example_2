4b5053a328b719818992db932826717a36dd49f1
Moonenm <u0157634@thomasmore.be>
2022-11-12 13:19:15 +0100

    edit routes.js add index

--- src/routes.js
+++ src/routes.js
@@ -13,14 +13,13 @@ let fs = require('fs');
  */
 
  router.get('/index', function(req, res){
-  res.writeHead(200, {
-    'Content-Type': 'text/html'
-});
-fs.readFile('./index.html', null, function (error, data) {
+
+  fs.readFile('./index.html', null, function (error, data) {
     if (error) {
         res.writeHead(404);
         res.write('Whoops! File not found!');
     } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
     }
     res.end();
