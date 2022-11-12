43d308d8afa8d7c0808c931712766929c4922739
Moonenm <u0157634@thomasmore.be>
2022-11-12 10:02:30 +0100

    edit test add first campus

--- test/test.js
+++ test/test.js
@@ -1,12 +1,14 @@
-const express = require('express');
 
 const request =require ('supertest');
 
 describe('get campus', () => {
-    it('should get all campussen', async () => {
+    it('should return first campus', async () => {
      await request('https://campus-api-example.netlify.app')
         .get('/.netlify/functions/api/campus')
         .expect(200)
         .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect((res) => {
+            console.log(res.body[0])
+           })
     });
 });
