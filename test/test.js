92d3545327d4781bb62f7629e0cd390d008c9be3
Moonenm <u0157634@thomasmore.be>
2022-11-12 08:34:04 +0100

    edit test

--- test/test.js
+++ test/test.js
@@ -4,8 +4,8 @@ const request =require ('supertest');
 
 describe('get campus', () => {
     it('should get all campussen', function(done) {
-        request('https://campus-api-example.netlify.app/')
-        .get('.netlify/functions/api/campus')
+        request('https://campus-api-example.netlify.app')
+        .get('/.netlify/functions/api/campus')
         .expect(200)
         .expect('Content-Type', 'application/json')
         .expect(/{"message":".*","status":"success"}/,done);
